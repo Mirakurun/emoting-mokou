30b7b968731de594aa75844b05784bb6934f0601
Mirakurun <25449974+Mirakurun@users.noreply.github.com>
2020-03-11 04:08:31 -0700

    Update constructor to include subdomain configuration

--- api/services/twitter.js
+++ api/services/twitter.js
@@ -1,8 +1,9 @@
 const Twitter = require('twitter-lite');
 
-module.exports.twitter = req => {
+module.exports.twitter = ({ req, subdomain = 'api' }) => {
   const { token, tokenSecret } = req.user;
   return new Twitter({
+    subdomain,
     consumer_key: process.env.CONSUMER_KEY,
     consumer_secret: process.env.CONSUMER_SECRET,
     access_token_key: token,
