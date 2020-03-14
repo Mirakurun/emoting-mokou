b656ff348e0a7b82dc933535cd2dc56b7510ee84
Mirakurun <25449974+Mirakurun@users.noreply.github.com>
2020-03-14 01:44:25 -0700

    Remove firestore connection

--- api/db/firestore.js
+++
@@ -1,8 +0,0 @@
-const { Firestore } = require('@google-cloud/firestore');
-
-const db = new Firestore({
-  projectId: process.env.GCLOUD_PROJECT,
-  keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
-});
-
-module.exports = db;
