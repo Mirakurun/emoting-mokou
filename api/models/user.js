a8a0e2033a965cf3b14f18c9d0d55a0f11df8f8b
Mirakurun <25449974+Mirakurun@users.noreply.github.com>
2020-03-22 19:48:25 -0700

    Update user model

--- api/models/user.js
+++ api/models/user.js
@@ -2,14 +2,18 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
-  displayName: { type: String, required: true },
-  photos: [{ value: { type: String, required: true } }],
-  provider: { type: String, required: true },
-  token: { type: String },
-  tokenSecret: { type: String },
-  uid: { type: String, required: true },
-  username: { type: String, required: true },
-});
+const userSchema = new Schema(
+  {
+    displayName: { type: String, required: true },
+    photos: [{ value: { type: String, required: true } }],
+    provider: { type: String, required: true },
+    role: { type: String, default: 'user' },
+    token: { type: String },
+    tokenSecret: { type: String },
+    uid: { type: String, required: true },
+    username: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('User', userSchema);
