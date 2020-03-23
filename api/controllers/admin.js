d2ff4b3cbea87d789294bfd4abfa9e1c600ea0f0
Mirakurun <25449974+Mirakurun@users.noreply.github.com>
2020-03-22 19:58:01 -0700

    Add admin controller

---
+++ api/controllers/admin.js
@@ -0,0 +1,81 @@
+const AWS = require('aws-sdk');
+const Emote = require('../models/emote');
+
+AWS.config.getCredentials(err => {
+  if (err) console.log(err.stack);
+  else {
+    console.log('Access key:', AWS.config.credentials.accessKeyId);
+    console.log('Secret access key:', AWS.config.credentials.secretAccessKey);
+  }
+});
+
+AWS.config.update({ region: process.env.REGION });
+
+const s3 = new AWS.S3();
+
+exports.addEmote = async (req, res, next) => {
+  const { description, filename, tags } = req.body;
+
+  try {
+    console.log('Adding emote to database...');
+
+    const emote = new Emote({
+      description,
+      filename,
+      tags,
+    });
+
+    const data = await emote.save();
+
+    console.log('Added emote.');
+
+    res.status(201).json(data);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.updateEmote = async (req, res, next) => {
+  const { id } = req.params;
+  const update = req.body;
+
+  try {
+    console.log('Updating emote...');
+
+    const emote = await Emote.findByIdAndUpdate(
+      id,
+      { $set: update },
+      { new: true }
+    );
+
+    console.log('Updated emote.');
+
+    res.status(200).json(emote);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.createPresignedPost = (req, res, next) => {
+  const { filename } = req.body;
+
+  console.log('Creating pre-signed POST...');
+
+  const params = {
+    Bucket: process.env.BUCKET,
+    Expires: 60,
+    Fields: {
+      key: `${process.env.KEYPREFIX}${filename}`,
+    },
+  };
+
+  s3.createPresignedPost(params, (err, data) => {
+    if (err) {
+      next(err);
+    } else {
+      console.log('Created pre-signed POST.');
+
+      res.status(200).json(data);
+    }
+  });
+};
