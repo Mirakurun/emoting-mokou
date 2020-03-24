a68ac6da1d4a86f29077d9b66430b9e9a9e48efb
Mirakurun <25449974+Mirakurun@users.noreply.github.com>
2020-03-24 00:19:50 -0700

    Add addToFavorites() and removeFromFavorites()

--- api/models/user.js
+++ api/models/user.js
@@ -5,6 +5,7 @@ const { Schema } = mongoose;
 const userSchema = new Schema(
   {
     displayName: { type: String, required: true },
+    favorites: [{ type: Schema.Types.ObjectId, ref: 'Emote' }],
     photos: [{ value: { type: String, required: true } }],
     provider: { type: String, required: true },
     role: { type: String, default: 'user' },
@@ -15,5 +16,28 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
+
+userSchema.methods.addToFavorites = function(emote) {
+  const { _id: id } = emote;
+
+  if (!this.favorites.includes(id)) {
+    this.favorites.push(id);
+  }
+
+  return this.save();
+};
+
+userSchema.methods.removeFromFavorites = function(emote) {
+  const { _id: id } = emote;
+
+  const index = this.favorites.indexOf(id);
+
+  if (index > -1) {
+    this.favorites(index, 1);
+    return this.save();
+  }
+
+  return Promise.reject(new Error('Favorite not found!'));
+};
 
 module.exports = mongoose.model('User', userSchema);
