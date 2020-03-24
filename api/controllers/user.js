a459dcb2c80be772ee17c8fb7d34981e653ebcee
Mirakurun <25449974+Mirakurun@users.noreply.github.com>
2020-03-24 01:04:54 -0700

    Add user controller

---
+++ api/controllers/user.js
@@ -0,0 +1,35 @@
+const Emote = require('../models/emote');
+
+exports.addToFavorites = async (req, res, next) => {
+  const { emoteId } = req.body;
+
+  try {
+    const emote = await Emote.findById(emoteId);
+
+    if (!emote) {
+      res.status(422).json({ message: 'Emote not found.' });
+    }
+
+    await req.user.addToFavorites(emote);
+
+    console.log('Added emote to favorites.');
+
+    res.status(204).send();
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.removeFromFavorties = async (req, res, next) => {
+  const { emoteId } = req.body;
+
+  try {
+    await req.user.removeFromFavorties(emoteId);
+
+    console.log('Removed emote from favorite.');
+
+    res.status(200).send();
+  } catch (error) {
+    next(error);
+  }
+};
