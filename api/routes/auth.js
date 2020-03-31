f37a7e52b82ee9880236becd613ea833f6efc513
Mirakurun <25449974+Mirakurun@users.noreply.github.com>
2020-03-30 17:02:06 -0700

    Update twitter auth callback

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -2,16 +2,20 @@ const express = require('express');
 const passportTwitter = require('../config/passport');
 const authController = require('../controllers/auth');
 
+const app = express();
 const router = express.Router();
 
 router.get('/twitter', passportTwitter.authenticate('twitter'));
 
 router.get(
   '/twitter/callback',
-  passportTwitter.authenticate('twitter', {
-    successRedirect: '/',
-    failureRedirect: '/login',
-  })
+  passportTwitter.authenticate('twitter', { failureRedirect: '/' }),
+  (req, res) => {
+    if (app.get('env') === 'production') {
+      return res.redirect('/');
+    }
+    return res.redirect('http://localhost:8080');
+  }
 );
 
 router.get('/logout', authController.logout);
