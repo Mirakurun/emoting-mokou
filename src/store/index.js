1d0d24f8cd4b63f7a1fd4f8aa3cf558db1e09cb9
Mirakurun <25449974+Mirakurun@users.noreply.github.com>
2020-04-01 23:51:19 -0700

    Import user store

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
-// import example from './module-example'
+import user from './user';
 
 Vue.use(Vuex);
 
@@ -14,10 +14,10 @@ Vue.use(Vuex);
  * with the Store instance.
  */
 
-export default function (/* { ssrContext } */) {
+export default function(/* { ssrContext } */) {
   const Store = new Vuex.Store({
     modules: {
-      // example
+      user,
     },
 
     // enable strict mode (adds overhead!)
