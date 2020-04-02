6a4c2b2b6f2b726dc5825f5c9d764106b8428a08
Mirakurun <25449974+Mirakurun@users.noreply.github.com>
2020-04-01 23:42:43 -0700

    Add axios instance

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -2,3 +2,12 @@ import Vue from 'vue';
 import axios from 'axios';
 
 Vue.prototype.$axios = axios;
+
+const axiosInstance = axios.create({
+  baseURL: process.env.API,
+  withCredentials: true,
+});
+
+Vue.prototype.$axios = axiosInstance;
+
+export { axiosInstance };
