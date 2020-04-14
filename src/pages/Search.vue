9d92f15c4d76ab35b7a557de9947f2059ea1a864
Mirakurun <25449974+Mirakurun@users.noreply.github.com>
2020-04-14 02:59:45 -0700

    Add search page

---
+++ src/pages/Search.vue
@@ -0,0 +1,75 @@
+<template>
+  <q-page padding>
+    <div class="row">
+      <div class="col-12 col-sm-12 col-md-9 col-lg-10">
+        <div class="row">
+          <div class="col">
+            <q-input
+              v-model="search"
+              placeholder="Search emotes..."
+              dense
+              rounded
+              outlined
+              @keyup.enter="onSearch(search)"
+            >
+              <template #append>
+                <q-icon
+                  class="cursor-pointer"
+                  name="fas fa-search"
+                  @click="onSearch(search)"
+                />
+              </template>
+            </q-input>
+          </div>
+        </div>
+        <div class="row q-pt-md q-col-gutter-md">
+          <div
+            v-for="(emote, index) in data"
+            :key="index"
+            class="col-xs-6 col-sm-4 col-md-3 col-lg-2"
+          >
+            <emote-card v-bind="emote" />
+          </div>
+        </div>
+      </div>
+      <div class="col-12 gt-sm col-md-3 col-lg-2">twitter</div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import EmoteCard from 'components/EmoteCard';
+
+export default {
+  name: 'PageSearch',
+  components: {
+    EmoteCard,
+  },
+  props: {
+    query: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      search: '',
+      data: [],
+    };
+  },
+  created() {
+    this.onSearch(this.query);
+  },
+  methods: {
+    async onSearch(query) {
+      try {
+        const { data } = await this.$axios.get(`emote?query=${query}`);
+
+        this.data = data;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+};
+</script>
