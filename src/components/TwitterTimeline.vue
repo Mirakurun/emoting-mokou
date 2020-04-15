9a7a75d180b7f1efc975bcce12bbba7e0e202a67
Mirakurun <25449974+Mirakurun@users.noreply.github.com>
2020-04-14 22:06:05 -0700

    Add 2 new components

---
+++ src/components/TwitterTimeline.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="q-px-md">
+    <div class="text-subtitle1 text-grey-14">Meet the artist</div>
+    <a
+      class="twitter-timeline"
+      data-height="100vh"
+      :data-theme="$store.state.user.darkMode ? 'dark' : 'light'"
+      href="https://twitter.com/jokanhiyou?ref_src=twsrc%5Etfw"
+      >Tweets by jokanhiyou</a
+    >
+    <div ref="timeline"></div>
+  </div>
+</template>
+
+<script>
+let tweets = null;
+
+export default {
+  name: 'TwitterTimeline',
+  created() {
+    this.createScriptTag();
+  },
+  mounted() {
+    this.$refs.timeline.appendChild(tweets);
+  },
+  methods: {
+    createScriptTag() {
+      tweets = document.createElement('script');
+      tweets.setAttribute('src', 'https://platform.twitter.com/widgets.js');
+      tweets.setAttribute('charset', 'utf-8');
+      tweets.setAttribute('async', true);
+    },
+  },
+};
+</script>
