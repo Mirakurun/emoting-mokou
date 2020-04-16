e197afc5c6bd2f512163c3b39955a8d02823be7e
Mirakurun <25449974+Mirakurun@users.noreply.github.com>
2020-04-15 22:37:11 -0700

    Fix for #13

--- src/store/user/actions.js
+++ src/store/user/actions.js
@@ -12,21 +12,6 @@ export async function fetchFavorites({ commit }) {
   }
 }
 
-export async function fetchUser({ commit }) {
-  try {
-    const { data, status } = await axiosInstance.get('/user/profile');
-
-    if (status === 200) {
-      commit('setUser', data);
-    }
-    if (status === 401) {
-      commit('clearUser');
-    }
-  } catch (error) {
-    console.error(error);
-  }
-}
-
 export async function setDarkMode({ commit }, payload) {
   try {
     const { data, status } = await axiosInstance.post('/user/darkmode', {
