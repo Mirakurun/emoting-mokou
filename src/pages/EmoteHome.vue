f0d54b4fd9ed34a180d1ae0115d6964a9eb7af2c
Mirakurun <25449974+Mirakurun@users.noreply.github.com>
2020-04-24 01:35:29 -0700

    Update emote home page

--- src/pages/EmoteHome.vue
+++ src/pages/EmoteHome.vue
@@ -4,16 +4,20 @@
       <div class="col-12 col-sm-12 col-md-9 col-lg-6 offset-lg-2">
         <q-card :bordered="$q.dark.isActive">
           <q-img
-            :alt="data.caption"
+            :alt="emote.caption"
             contain
             :class="$q.dark.isActive ? 'bg-grey-9' : 'bg-grey-1'"
-            :src="`statics/images/emotes/${data.filename}`"
+            :src="`statics/images/emotes/${emote.filename}`"
             :ratio="4 / 3"
             style="max-height: 400px"
           />
           <q-card-actions align="right">
-            <add-to-favorites-btn :id="data.id" flat label="Add to Favorites" />
-            <add-to-tweet-btn :id="data.id" flat label="Add to Tweet" />
+            <add-to-favorites-btn
+              :id="emote.id"
+              flat
+              label="Add to Favorites"
+            />
+            <add-to-tweet-btn :id="emote.id" flat label="Add to Tweet" />
             <q-btn
               :color="$q.dark.isActive ? 'cyan' : 'blue'"
               flat
@@ -21,17 +25,17 @@
               label="fullscreen"
               @click="onToggle"
             />
-            <edit-emote-btn :id="data.id" flat label="Edit" />
+            <edit-emote-btn :id="emote.id" flat label="Edit" />
           </q-card-actions>
           <q-card-section class="text-h6 caption">
-            <div>{{ data.caption }}</div>
+            <div>{{ emote.caption }}</div>
             <div class="text-subtitle2 text-grey">
               Uploaded on {{ formatCreatedAt }}
             </div>
           </q-card-section>
           <q-card-section>
             <q-chip
-              v-for="(tag, i) in data.tags"
+              v-for="(tag, i) in emote.tags"
               :key="i"
               :label="tag"
               :title="tag"
@@ -47,19 +51,19 @@
     <fullscreen-emote
       id="fullscreen"
       ref="fullscreen"
-      :filename="data.filename"
+      :filename="emote.filename"
     />
   </q-page>
 </template>
 
 <script>
-import { axiosInstance } from 'boot/axios';
 import AddToFavoritesBtn from 'components/AddToFavoritesBtn';
 import AddToTweetBtn from 'components/AddToTweetBtn';
 import EditEmoteBtn from 'components/EditEmoteBtn';
 import FullscreenEmote from 'components/FullscreenEmote';
 import TwitterTimeline from 'components/TwitterTimeline';
 import { timelineMixin } from 'mixins/timeline';
+import { emoteMixin } from 'mixins/emote';
 import { format } from 'date-fns';
 
 export default {
@@ -71,37 +75,16 @@ export default {
     FullscreenEmote,
     TwitterTimeline,
   },
-  mixins: [timelineMixin],
-  data() {
-    return {
-      data: {},
-    };
-  },
+  mixins: [emoteMixin, timelineMixin],
   computed: {
     formatCreatedAt() {
-      if (this.data.createdAt) {
-        return format(new Date(this.data.createdAt), 'PPP');
+      if (this.emote.createdAt) {
+        return format(new Date(this.emote.createdAt), 'PPP');
       }
       return null;
     },
   },
-  created() {
-    this.fetchEmote();
-  },
   methods: {
-    async fetchEmote() {
-      try {
-        const { data, status } = await axiosInstance.get(
-          `/emote/${this.$route.params.id}`
-        );
-
-        if (status === 200) {
-          this.data = data;
-        }
-      } catch (error) {
-        console.error(error);
-      }
-    },
     onToggle() {
       this.$refs.fullscreen.toggle();
       const target = this.$el.querySelector('#fullscreen');
