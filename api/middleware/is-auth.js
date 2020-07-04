15bfaee95a4bf794a0a10cdb64624b121ac72553
Mirakurun <25449974+Mirakurun@users.noreply.github.com>
2020-07-03 18:21:08 -0700

    Update auth middelware

--- api/middleware/is-auth.js
+++ api/middleware/is-auth.js
@@ -1,4 +1,7 @@
 module.exports = (req, res, next) => {
+  if (!req.user && req.path === '/profile') {
+    return res.status(204).send();
+  }
   if (!req.user) {
     return res.status(401).send();
   }
