82f935d26ed3906d14012e7ac2aec5b002c0a0de
Mirakurun <25449974+Mirakurun@users.noreply.github.com>
2020-07-23 02:14:26 -0700

    Update twitter controller

--- api/controllers/twitter.js
+++ api/controllers/twitter.js
@@ -7,17 +7,25 @@ exports.tweet = async (req, res, next) => {
   const { text } = req.body;
 
   try {
+    console.log(req.files);
+
     const promises = files.map(file => {
       return uploadClient.post('media/upload', {
         media_data: file.buffer.toString('base64'),
       });
     });
 
+    console.log(promises);
+
     const responses = await Promise.all(promises);
+
+    console.log(responses);
 
     const medias = responses.map(response => {
       return response.media_id_string;
     });
+
+    console.log(medias);
 
     const tweet = await client.post('statuses/update', {
       status: text,
@@ -29,6 +37,8 @@ exports.tweet = async (req, res, next) => {
     await req.user.clearMedia();
 
     console.log('Cleared emotes.');
+
+    console.log(tweet);
 
     const { url } = tweet.entities.media[0];
 
