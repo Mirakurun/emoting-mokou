c6c317fdc39ca70ba70574e3f9d641d9c29191c3
Kevin Chhay <25449974+Mirakurun@users.noreply.github.com>
2023-06-28 22:16:11 -0700

    update to v2

--- api/services/twitter.js
+++ api/services/twitter.js
@@ -3,6 +3,8 @@ const Twitter = require('twitter-lite');
 module.exports.twitter = ({ req, subdomain = 'api' }) => {
   const { token, tokenSecret } = req.user;
   return new Twitter({
+    version: "2",
+    extension: false,
     subdomain,
     consumer_key: process.env.CONSUMER_KEY,
     consumer_secret: process.env.CONSUMER_SECRET,
